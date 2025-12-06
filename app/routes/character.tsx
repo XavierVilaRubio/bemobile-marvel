f17a40846ab1932d7b745db13aa5d5d88db1b972
Xavier Vila Rubio <xavier.vila@dribba.com>
2025-12-06 12:25:49 +0100

    feat: enhance Character component with favorite button and improved layout for better user experience

--- app/routes/character.tsx
+++ app/routes/character.tsx
@@ -1,3 +1,4 @@
+import FavButton from "~/components/ui/fav-button";
 import { getCharacter } from "~/services/api";
 import type { Route } from "./+types/character";
 
@@ -18,15 +19,31 @@ export default function Character({ loaderData }: Route.ComponentProps) {
 				} as React.CSSProperties
 			}
 		>
-			<div className="container m-auto flex max-w-2/5 gap-12">
+			<div className="container m-auto flex flex-col lg:max-w-1/2 lg:flex-row lg:gap-12">
 				<img
 					src={character.images.lg}
 					alt={character.name}
-					className="aspect-square w-full max-w-80 object-cover"
+					className="aspect-square w-full object-cover lg:max-w-80"
 				/>
-				<div>
-					<h1 className="font-bold text-4xl uppercase">{character.name}</h1>
-					{/* <p className="text-sm">{character.biography.fullName}</p> */}
+				<div className="my-auto space-y-6 px-4 pt-6 pb-12 lg:p-0">
+					<div className="flex items-center justify-between">
+						<h1 className="font-bold text-3xl uppercase lg:text-4xl">
+							{character.name}
+						</h1>
+						<FavButton
+							characterId={character.id}
+							className="size-6"
+							variant="inverted"
+						/>
+					</div>
+					<p>
+						Know as {character.biography.fullName}, born in{" "}
+						{character.biography.placeOfBirth}. First appearance in{" "}
+						{character.biography.firstAppearance}. Publisher:{" "}
+						{character.biography.publisher}. Works as {character.work.base}.
+						Connected to groups: {character.connections.groupAffiliation}.
+						{/* Relatives: {character.connections.relatives}. */}
+					</p>
 				</div>
 			</div>
 		</div>
