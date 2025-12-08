97bb73f340846b579ad7a21845f7dc0d301100e5
Xavier Vila Rubio <xavier.vila@dribba.com>
2025-12-08 12:30:32 +0100

    feat: refactor FavLink to separate content display and improve routing logic for character pages

--- app/components/ui/fav-link.tsx
+++ app/components/ui/fav-link.tsx
@@ -1,16 +1,34 @@
 import { useEffect, useState } from "react";
-import { useSearchParams } from "react-router";
+import { Link, useLocation, useSearchParams } from "react-router";
 import { useLocalStorage } from "usehooks-ts";
 import FavIcon from "../icons/fav-icon";
 
-const FavLink = () => {
+const Content = () => {
 	const [favs] = useLocalStorage<number[]>("favs-characters", []);
 	const [favsCount, setFavsCount] = useState(0);
 	useEffect(() => {
 		setFavsCount(favs.length);
 	}, [favs]);
+	return (
+		<>
+			<FavIcon />
+			<span className="text-white">{favsCount}</span>
+		</>
+	);
+};
+
+const FavLink = () => {
+	const location = useLocation();
+	const isCharacterPage = location.pathname.includes("character");
 
 	const [, setSearchParams] = useSearchParams();
+
+	if (isCharacterPage)
+		return (
+			<Link to="/?favs=true" className="flex cursor-pointer items-center gap-2">
+				<Content />
+			</Link>
+		);
 
 	return (
 		<button
@@ -18,8 +36,7 @@ const FavLink = () => {
 			className="flex cursor-pointer items-center gap-2"
 			onClick={() => setSearchParams({ favs: "true" })}
 		>
-			<FavIcon />
-			<span className="text-white">{favsCount}</span>
+			<Content />
 		</button>
 	);
 };
